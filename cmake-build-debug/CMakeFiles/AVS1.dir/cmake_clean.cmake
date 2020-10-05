0c3098923c4af5f8a6fe3ba5becef3d400c576e6
Alex168 <sashok1356@gmail.com>
2020-10-05 18:28:34 +0300

    initial commit

---
+++ cmake-build-debug/CMakeFiles/AVS1.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "AVS1"
+  "AVS1.pdb"
+  "CMakeFiles/AVS1.dir/main.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/AVS1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
